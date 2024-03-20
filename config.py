17e9ff7bfb371bf3d0fb3a7e8512e0ec3efda54d
spirovskib <spirovski.b@gmail.com>
2024-03-20 09:45:47 +0100

    test: another push of secret

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 
 
+AWS_USER = 'AKIA2BYS3MX7V3RNOD5W'
+AWS_PASS = '+TmOex/50UtdlnJcFOo7rhaY0/7iU4QyBaxVyRZ9'
 
 vuln_app = connexion.App(__name__, specification_dir='./openapi_specs')
 
