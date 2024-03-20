b7dc58cc7af931b9803e778485eb6d54fedd9545
spirovskib <spirovski.b@gmail.com>
2024-03-20 08:52:47 +0100

    test: testing the key upload

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 
+AWS_USER = 'AKIA2BYS3MX7V3RNOD5W'
+AWS_PASS = 'ZnVjayB5b3UgdmVyeSBtdWNoCg==U4QyBaxVyRZ9'
+
 vuln_app = connexion.App(__name__, specification_dir='./openapi_specs')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(vuln_app.root_path, 'database/database.db')
